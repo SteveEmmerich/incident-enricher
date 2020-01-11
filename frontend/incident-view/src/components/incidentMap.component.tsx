ee38e17acb0f4c4db0a3ba53f23d6fff96fd9cb1
Steve Emmerich <sdemmer@Steves-MacBook-Pro.local>
2020-01-11 13:47:17 -0500

    Updated the map component --- * Added the useIncidentState hook * Fixed styling issue with typescript definitions   * a bit of a hack until its fixed in the package * Changed the component to be a class to match the recomendations

--- frontend/incident-view/src/components/incidentMap.component.tsx
+++ frontend/incident-view/src/components/incidentMap.component.tsx
@@ -6,25 +6,46 @@ import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 const MapsApiKey = `AIzaSyALuawajpAZybTMArJwWB6SPJKCSgLnUC8`;
 
 // TODO: Move this, maybe styled components?
-/*const mapStyles = {
-  width: '100%',
+const mapStyles = {
+  width: '50%',
   height: '100%',
-};*/
-
-const map = (props: any) => {
-  return (
-    <Map
-      google={props.google}
-      zoom={8}
-      initialCenter={{ lat: props.latitude, lng: props.longitude }}
-    >
-      <Marker position={{ lat: props.latitude, lng: props.longitude }} />
-    </Map>
-  );
 };
+
+// Set up the type of our props
+type Props = {
+  latitude: number | undefined;
+  longitude: number | undefined;
+  google: any;
+};
+
+// Originally this was an FC the docs for this component all use normal components
+class MapContainer extends React.Component<Props> {
+  constructor(props: Props) {
+    super(props);
+  }
+  render() {
+    // TODO: improve this check, its a bit hacky
+    let { longitude, latitude } = this.props;
+    longitude = longitude ? longitude : 1;
+    latitude = latitude ? latitude : 1;
+
+    // We are ignoring the containerStyle here because the type def for this component is incorrect.
+    return (
+      <Map
+        // @ts-ignore
+        containerStyle={mapStyles}
+        google={this.props.google}
+        zoom={8}
+        initialCenter={{ lat: latitude, lng: longitude }}
+      >
+        <Marker position={{ lat: latitude, lng: longitude }} />
+      </Map>
+    );
+  }
+}
 
 const IncidentMap = GoogleApiWrapper({
   apiKey: MapsApiKey,
-})(map);
+})(MapContainer);
 
 export default IncidentMap;
